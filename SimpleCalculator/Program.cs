8f1c1284c80e9bc64d4a2b584ee480cdf72b67f8
Ryan B <admirallasers@gmail.com>
2016-08-15 20:06:42 -0500

    now captures numbers and operator

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,17 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            int counter = 1;
+            string prompt = "["+ counter + "]> ";
+
+            //step 1- pass a number to expression class
+            Console.WriteLine("Simple Calculato!!");
+            Console.Write(prompt);
+            counter++;
+            string userInput = Console.ReadLine();
+            Expression newExpression = new Expression(userInput);
+            Console.WriteLine(newExpression.firstTerm + newExpression._operator + newExpression.secondTerm);
+            Console.ReadLine();
         }
     }
 }
